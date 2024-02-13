2a7ed69cb3d8842004feda1b68d2d3880e2c44fe
ramune <12h.abba.y21@gmail.com>
2024-02-13 14:05:32 +0000

    ✅ test: encryptorへのテスト追加&不要なエラー処理の削除(100%エラーが発生しないことをライブラリから確認済み)

--- linebot/security/encryptor.go
+++ linebot/security/encryptor.go
@@ -2,7 +2,6 @@ package security
 
 import (
 	"fmt"
-	"linebot/logger"
 	"linebot/props"
 
 	"golang.org/x/crypto/scrypt"
@@ -15,9 +14,7 @@ type Encryptor interface {
 type EncryptorImpl struct{}
 
 func (e EncryptorImpl) SaltHash(value string) string {
-	ret, err := scrypt.Key([]byte(value), []byte(props.Salt), 32768, 8, 1, 32)
-	if err != nil {
-		logger.FatalWithStackTrace(err, &logger.LBFT900001)
-	}
+	ret, _ := scrypt.Key([]byte(value), []byte(props.Salt), 32768, 8, 1, 32)
+
 	return fmt.Sprintf("%x", ret)
 }
