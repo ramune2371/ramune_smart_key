3714a020d4c8d6c6383e4fbab8356a82dec01d13
ramune <12h.abba.y21@gmail.com>
2024-02-18 03:47:03 +0000

    🥅 error: 暗号化失敗時のエラー処理の変更

--- linebot/security/encryptor.go
+++ linebot/security/encryptor.go
@@ -2,7 +2,9 @@ package security
 
 import (
 	"fmt"
+	"linebot/logger"
 	"linebot/props"
+	"os"
 
 	"golang.org/x/crypto/scrypt"
 )
@@ -14,7 +16,11 @@ type Encryptor interface {
 type EncryptorImpl struct{}
 
 func (e EncryptorImpl) SaltHash(value string) string {
-	ret, _ := scrypt.Key([]byte(value), []byte(props.Salt), 32768, 8, 1, 32)
+	ret, err := scrypt.Key([]byte(value), []byte(props.Salt), 32768, 8, 1, 32)
+	if err != nil {
+		logger.FatalWithStackTrace(err, &logger.LBFT900001)
+		os.Exit(1)
+	}
 
 	return fmt.Sprintf("%x", ret)
 }
